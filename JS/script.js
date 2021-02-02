7e58566930b0140d4ca4a7164da27027277c25f4
Nicolas <juann.ruiz@upb.edu.co>
2021-02-02 01:14:29 -0500

    Control de campos llenos para login y registro

--- JS/script.js
+++ JS/script.js
@@ -1,77 +1,75 @@
+let Userlist = [];
 
-let Userlist =[];
-let a = document.getElementById('a-submit-registro');
-if(a){
-    a.addEventListener('click',registerNewUser);
+let a = document.getElementById("a-submit-registro");
+if (a) {
+  a.addEventListener("click", registerNewUser);
 }
-let b = document.getElementById('enter-login')
-if(b){
-    b.addEventListener('click',login);
-}
-function registerNewUser(){
-    
-    
-    let newUser = [], Username= '', Password= '', Email= '';
-
-    Username = document.getElementById('username-reg').value;
-    Password = document.getElementById('user-password-reg').value;
-    Email = document.getElementById('user-email-reg').value;
-
-    
-    
-    let pass=review(Username,Password);
-    if(pass==false){
-    newUser.push(Username,Password,Email);
-    addInUserlist(newUser);
-    }else{
-        alert('Usuario ya registrado');
-    }
-    
-    
-    
-    
-}
-function addInUserlist(pnewUser){
-    let Userlist=getUserlist();
-    Userlist.push(pnewUser);
-    localStorage.setItem('LSUserlist',JSON.stringify(Userlist));
-
-}
-function getUserlist(){
-    let LocalUserlist = JSON.parse(localStorage.getItem('LSUserlist'));
-    if(LocalUserlist == null){
-        LocalUserlist=Userlist;
-    }
-    return LocalUserlist;
-}
-function review(username,password){
-     alert('entra en revisión');
-     let Userlist = getUserlist();
-     let pass =false;
-     
-    for(let i=0;i < Userlist.length;i++){
-        if(username==Userlist[i][0]&&password==Userlist[i][1]){
-            pass = true;
-            alert('entro al if');
-        }
-        
-    }
-    return pass;
+let b = document.getElementById("enter-login");
+if (b) {
+  b.addEventListener("click", login);
 }
 
-function login(){
-    
-    let Username= '', Password= '';
-    
-    Username = document.getElementById('username').value;
-    Password = document.getElementById('user-password').value;
-    
+function registerNewUser() {
+  let newUser = [],
+    Username = "",
+    Password = "",
+    Email = "";
+
+  Username = document.getElementById("username-reg").value;
+  Password = document.getElementById("user-password-reg").value;
+  Email = document.getElementById("user-email-reg").value;
+
+  if (Username != "" && Password != "" && Email != "") {
     let pass = review(Username, Password);
-    if(pass==true){
-        alert('Bienvenido');
-    }else{
-        alert('Usuario no existe');
+    if (pass == false) {
+      newUser.push(Username, Password, Email);
+      addInUserlist(newUser);
+      window.location.href = "galeria.html";
+    } else {
+      alert("Usuario ya registrado");
     }
+  } else {
+    alert("Ingrese todos los campos");
+  }
+}
+function addInUserlist(pnewUser) {
+  let Userlist = getUserlist();
+  Userlist.push(pnewUser);
+  localStorage.setItem("LSUserlist", JSON.stringify(Userlist));
+}
+function getUserlist() {
+  let LocalUserlist = JSON.parse(localStorage.getItem("LSUserlist"));
+  if (LocalUserlist == null) {
+    LocalUserlist = Userlist;
+  }
+  return LocalUserlist;
+}
+function review(username, password) {
+  let Userlist = getUserlist();
+  let pass = false;
 
+  for (let i = 0; i < Userlist.length; i++) {
+    if (username == Userlist[i][0] && password == Userlist[i][1]) {
+      pass = true;
+    }
+  }
+  return pass;
 }
 
+function login() {
+  let Username = "",
+    Password = "";
+
+  Username = document.getElementById("username").value;
+  Password = document.getElementById("user-password").value;
+  if (Username != "" && Password != "") {
+    let pass = review(Username, Password);
+    if (pass == true) {
+      window.location.href = "galeria.html";
+    } else {
+      alert("Usuario no existe");
+    }
+  }else{
+      alert('Ingrese todos los campos');
+  }
+}
